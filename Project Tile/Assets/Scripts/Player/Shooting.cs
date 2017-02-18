a5253c23550cbafbe53e044921d1655303843491
Kerem Gökhan <mail@keremgokhan.net>
2017-02-18 22:43:08 +0100

    Add shooting

--- Project Tile/Assets/Scripts/Player/Shooting.cs
+++ Project Tile/Assets/Scripts/Player/Shooting.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour {
 
+    public GameObject projectilePrefab;
+    public Transform projectileSpawn;
+    public ushort projectileCount = 10;
+    public ushort projectileSpeed = 200;
+
+    private Queue<GameObject> projectiles;
+
 	// Use this for initialization
 	void Start () {
-		
+        initializeProjectiles();   
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.R)) {
+            reloadAmmo(this.projectileCount);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            fireProjectile();
+        }
+    }
+
+    private void reloadAmmo(ushort numberOfProjectiles) {
+        for (ushort i = 0; i < numberOfProjectiles; i++)
+        {
+            GameObject projectile = Instantiate(projectilePrefab);
+            this.projectiles.Enqueue(projectile);
+        }
+    }
+        
+    private void initializeProjectiles() {
+        this.projectiles = new Queue<GameObject>();
+
+        reloadAmmo(this.projectileCount);
+    }
+
+    private void fireProjectile() {
+        if (this.projectiles.Count <= 0)
+            return;
+
+        GameObject projectile = this.projectiles.Dequeue();
+        projectile.transform.position = projectileSpawn.position;
+        projectile.transform.rotation = projectileSpawn.rotation;
+        projectile.SetActive(true);
+        //projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * 6;
+        projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * projectileSpeed);
+    }
 }
